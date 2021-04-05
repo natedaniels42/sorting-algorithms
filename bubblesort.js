6e650cf704214cd14be8673b4cf932f44ea2f6c7
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-04-05 14:12:45 -0500

    Implemented bubblesort in bubblesort.js and added index.js

---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+const bubblesort = (arr) => {
+    let sorted = false;
+
+    while (!sorted) {
+        sorted = true;
+
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                const temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                sorted = false;
+            }
+        }
+    }
+    
+    return arr;
+}
+
+module.exports = bubblesort;
---
+++ index.js
@@ -0,0 +1,7 @@
+const bubblesort = require('./bubblesort');
+const mergesort = require('./mergesort');
+const quicksort = require('./quicksort');
+
+const arr = [3,76,44,28,99,12,36,55,108,92,20,0];
+
+//console.log(bubblesort(arr));
